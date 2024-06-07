5f53d83bc062acd73d7972efa5d1a8c525a71ea7
Kim Ha <thuyh3020@gmail.com>
2024-06-07 09:13:21 -0400

    Initial Commit

---
+++ purs/pursApp.swift
@@ -0,0 +1,17 @@
+//
+//  pursApp.swift
+//  purs
+//
+//  Created by Kim Thuy Ha on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct pursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
